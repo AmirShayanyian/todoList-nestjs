a8cbbd6054d0392deedf9e97ef2e40fde462d283
amirshayanyian <wahtamir12@gmail.com>
2024-06-29 23:53:26 +0330

    added typeOrm configurations

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,22 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TodoController } from './todo/todo.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 4942,
+      username: 'root',
+      password: '',
+      database: 'todoapp',
+      autoLoadEntities: true,
+      entities: [],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController, TodoController],
   providers: [AppService],
 })
--- src/todo/todo.controller.ts
+++ src/todo/todo.controller.ts
@@ -1,4 +1,6 @@
 import { Controller } from '@nestjs/common';
 
 @Controller('todo')
-export class TodoController {}
+export class TodoController {
+    
+}
